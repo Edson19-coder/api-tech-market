453115eff1bd0dbcd494399b0dff230b87993240
Edson Lugo <62279001+Edson19-coder@users.noreply.github.com>
2022-04-28 23:26:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var port = 5000;
 
 //Conexion DB
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/tech-market-db').then(()=>{
+mongoose.connect('mongodb+srv://admin:FMybogV2nHr8d0wr@cluster0.s4hym.mongodb.net/Cluster0?retryWrites=true&w=majority').then(()=>{
 		console.log("La conexion a la base de datos de Tech Market, se ha realizado correcatamente.");
 		//Crear servidor
 		app.listen(port, ()=>{
-			console.log("Servidor creado correcatamente en http://localhost:" + port);
+			console.log("Servidor creado correcatamente");
 		})
 	}).catch(err=> console.log(err));
